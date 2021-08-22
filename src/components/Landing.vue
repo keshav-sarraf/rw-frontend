fd7ae46d0fc82c4274264febfb758a1691507a61
keshav-sarraf <62810924+keshav-sarraf@users.noreply.github.com>
2021-08-23 01:28:57 +0530

    Empty Page
    Created empty page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,14 +1,50 @@
 <template>
-<div class="container  text-center mt-5 mb-5">
-    <h1 class="mt-5">Bootstrap 5 is working fine with Vuejs!!</h1>
-    <button class="btn btn-primary">Btn</button>
+<div class="container">
+
+    <h2>Past Posts</h2>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Date</th>
+                <th scope="col">Read Duration</th>
+                <th scope="col">Labels</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(post, index) in postsList" :key="post.title">
+                <th scope="row">{{index+1}}</th>
+                <td>
+                    <router-link v-bind:to=post.router_link>{{post.title}}</router-link>
+                </td>
+                <td>{{post.date}}</td>
+                <td>{{post.read_duration}}</td>
+                <td>
+                    <span v-for="label in post.labels" :key="label" class="badge bg-primary m-1">
+                        {{label}}
+                    </span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 </template>
 
 <script>
+import {
+    ref
+} from 'vue';
+import postsListJson from '../assets/posts/posts.json';
+
 export default {
     setup() {
+        const postsList = ref(postsListJson.posts);
 
+        return {
+            postsList
+        }
     },
 }
 </script>
---
+++ src/components/posts/RandNumGen.vue
@@ -0,0 +1,16 @@
+// Random Number Generator
+// 22nd August 2021
+
+<template>
+<div class="container">
+    <p>Random Number Generator</p>
+</div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+    },
+}
+</script>
