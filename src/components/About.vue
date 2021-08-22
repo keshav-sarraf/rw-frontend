cbf96709272c9f31fbcb95662eb374836f779ccd
keshav-sarraf <62810924+keshav-sarraf@users.noreply.github.com>
2021-08-22 19:43:13 +0530

    Added margin in about page
    added margin in about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,8 +3,8 @@
         <div v-for="chat in chatList" :key="chat.question">
             <!-- question -->
             <div class="row">
-                <div class="col-sm-8 offset-sm-2 align-self-start">
-                    <div class="card">
+                <div class="col-sm-8 offset-sm-2 mb-3 align-self-start">
+                    <div class="card text-white bg-dark">
                         <div class="card-body">
                             <h5 class="card-title">You</h5>
                             <p class="card-text">{{chat.question}}</p>
@@ -12,8 +12,8 @@
                     </div>
                 </div>
 
-                <div class="col-sm-8 offset-sm-4 align-self-end">
-                    <div class="card">
+                <div class="col-sm-8 offset-sm-4 mb-3 align-self-end">
+                    <div class="card border-primary">
                         <div class="card-body">
                             <h5 class="card-title">Reinventing Wheel</h5>
                             <p class="card-text">{{chat.answer}}</p>
@@ -22,7 +22,6 @@
                 </div>
             </div>    
             
-            <br>
         </div>
     </div>
 
