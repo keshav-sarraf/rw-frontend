bcc786d614794b45499cfb2e8155ed7e557223f9
keshav-sarraf <62810924+keshav-sarraf@users.noreply.github.com>
2021-09-04 10:29:39 +0530

    Typo correction

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -34,7 +34,7 @@
         </div>
         <div class="col">
             <p>
-                <a href="https://www.instagram.com/simple.explainations/">@simple.expainations</a>
+                <a href="https://www.instagram.com/simple.explainations/">@simple.explainations</a>
             </p>
         </div>
     </div>
