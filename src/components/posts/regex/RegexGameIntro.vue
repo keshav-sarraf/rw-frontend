33c5a6659c149e00249f8bbc4806a566cacf17e7
keshav-sarraf <62810924+keshav-sarraf@users.noreply.github.com>
2021-09-13 19:16:40 +0530

    finished intro text

--- src/components/posts/regex/RegexGameIntro.vue
+++ src/components/posts/regex/RegexGameIntro.vue
@@ -1,33 +1,54 @@
 <template>
 <div class="container">
-    
+
     <div class="row">
-        <div class="col-sm-4 text-start">
+        <!-- <div class="col-sm-4 text-start">
             Level 0 of 15
             <div class="progress">
                 <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 0%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
+        </div> -->
+
+        <div class="col-sm-4 text-start">
+            <h2>✉️ The unexpected email</h2>
         </div>
 
-        <div class="col-sm-4 text-center">
-            <h2>Introduction</h2>
-        </div>
-
-        <div class="col-sm-4 text-end">
+        <!-- <div class="col-sm-4 text-end">
             Time Left : {{Math.round(timeLeft)}}s
             <div class="progress">
                 <div class="progress-bar progress-bar-striped" role="progressbar" :style="{width : percentTimeLeft + '%'}" :class="{'bg-success': percentTimeLeft > 50,
                             'bg-warning' : percentTimeLeft > 20 && percentTimeLeft <= 50,
                             'bg-danger' : percentTimeLeft <= 20}" v-bind:aria-valuenow="Math.round(percentTimeLeft)" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
-        </div>
+        </div> -->
     </div>
 
     <hr>
 
-    <p>
-        Intro
-    </p>
+    <div>
+        <p>
+            Agent Brown,
+        </p>
+        <p>
+            The agency is in dire need of your help. We are working on bringing down a secret organization called "The Group". They want to take down the internet and bring chaos to the world. Your old partner - Agent White was successful in infiltrating The Group's inner circle, but before she could extract meaningful information, her cover was blown. She was successful in escaping but here current whereabouts are unknown and we have lost all contact.
+        </p>
+        <p>
+            Her last message has led us to a laptop. We suspect that the moment we turn on the laptop, it'll start a self destruct sequence and we won't have enough time to analyze everything. That's why we are reaching out to you. With your experience in RegEx, we may just be able to save the world and your friend's life.
+        </p>
+        <p>
+            Your mission, should you choose to accept it would be to analyze the documents present in the laptop and figure out The Group's next plan. Beware that time is of the essence.
+        </p>
+
+        <div class="row">
+            <div class="col-sm-2">
+                <button type="button" @click="$router.push('regex-game-intro')" class="btn btn-primary">Accept Mission</button>
+            </div>
+            <div class="col-sm-2">
+                <button type="button" @click="$router.push('regex-post-intro')" class="btn btn-danger">Reject Mission</button>
+            </div>
+        </div>
+
+    </div>
 </div>
 </template>
 
